c65bb9f8d26c66e4996d639a717f495b09da7633
Jure Bordon <jure.bordon@gmail.com>
2014-11-18 15:20:48 +0100

    Finished game!

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+		
+	// Use this for initialization
+
+	public Text guessText;
+
+	int max = 1000;
+	int min = 1;
+	int guess;
+	
+	void Start () {
+		
+		// Initilize printout and min/max
+		StartGame();
+			
+	}
+	
+	void StartGame() {
+
+		max = max + 1;
+		NextGuess ();
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess() {
+		//guess = (max+min)/2;
+		guess = Random.Range (min, max);
+		guessText.text = guess.ToString();
+		print ("Next guess is " + guess + ".");
+	}
+}
